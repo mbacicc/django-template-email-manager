4cf5b255345431f6b5e66cbe75370a9aa577435a
Francesco Caponio <mbacicc@gmail.com>
2021-04-24 08:54:27 +0200

    Initial Model definition and basic functions

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'template_email_manager',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- template_email_manager/views.py
+++ template_email_manager/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from .tasks import *
+
+def process_emails(request):
+    background_process_emails(schedule=0, repeat=60)
+    return HttpResponse('ok')
+    
+def add_test_email(request):
+    result = task_add_test_email()
+    return HttpResponse(result)
