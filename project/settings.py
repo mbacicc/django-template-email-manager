ac2a9f4ffb8b459600c4aa5676d456ed7a334980
Francesco Caponio <mbacicc@gmail.com>
2021-04-24 08:23:20 +0200

    Added admindocs

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5grtelxpei0!e#z=#x$dlcjq+8ox^8e16m=h2vp8vclov@m^&i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
+    
 ]
 
 MIDDLEWARE = [
